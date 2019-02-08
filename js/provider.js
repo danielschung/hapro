15c1199937198a9eacaf6f00ac9792f93852d023
Daniel.Chung <dchung@NS72649-HA.local>
2019-02-08 15:03:51 -0600

    initial push

---
+++ js/provider.js
@@ -0,0 +1,21 @@
+$(document).ready (function() {
+
+	$("div.provider div.accordian h4").on('click', function() {
+
+		const c = $(this).next();
+		const plus = $(this).find('i:nth-child(1)');
+		const minus = $(this).find('i:nth-child(2)');
+
+		c.slideToggle(300);
+
+		if (plus.is(':visible')) {
+			plus.hide();
+			minus.fadeIn(200);
+		} else {
+			minus.hide();
+			plus.fadeIn(200);
+		};
+
+	});
+
+});
